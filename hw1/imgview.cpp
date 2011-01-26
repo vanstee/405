e8d7141c9d3b7710f78ece4015f429835f9b5322
Patrick Van Stee <patrickvanstee@gmail.com>
2011-01-25 22:19:07 -0500

    add a description to hw1

--- hw1/imgview.cpp
+++ hw1/imgview.cpp
@@ -1,7 +1,17 @@
-// description
-// name
-// date
-// instructions
+// Description:
+// This is a solution to homework project 1 with both basic requirements 
+// and advanced extension implemented.
+//
+// Name: Patrick Van Stee
+//
+// Date: Javuary 25, 2011
+//
+// Instructions:
+// $ make clean && make
+// $ ./imgview or ./imgview image.jpg
+// Press r to read and display a new image form a file
+// Press w to write the currently displayed image to a file
+// Press q or ESC to quit the program
 
 #include <Magick++.h>
 #include <cstdlib>
