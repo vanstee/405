83386e933e4092b6065304182abf281b9d69fcec
Patrick Van Stee <patrickvanstee@gmail.com>
2011-03-21 16:09:56 -0400

    add motion rotation

--- hw5/viewer.cpp
+++ hw5/viewer.cpp
@@ -4,24 +4,47 @@
 #  include <GL/glut.h>
 #endif
 
-#define WIDTH  800
-#define HEIGHT 600
-
 using namespace std;
+
+#define WIDTH       800
+#define HEIGHT      600
+#define ROTFACTOR   0.2
+#define XLATEFACTOR 0.5
+
+#define ABS(x) x < 0 ? -x : x
+
+static double pan      = 0;
+static double tilt     = 0;
+static double approach = 0;
+static double thetax   = 0;
+static double thetay   = 0;
+static int mousex      = 0;
+static int mousey      = 0;
+static int button      = -1;
+
 
 void init() {
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   glClearColor(0, 0, 0, 1);
+  glEnable(GL_DEPTH_TEST);  
   glEnable(GL_LIGHTING);
   glEnable(GL_LIGHT0);
 }
 
 void display() {
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glLoadIdentity();
-  gluLookAt (0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);  
-  glutSolidCube(2);
+  gluLookAt (0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);      
+  
+  glRotatef(tilt, 1, 0, 0);
+  glRotatef(pan, 0, 1, 0);
+  glTranslatef(0, 0, approach);
+  glRotatef(thetay, 0, 1, 0);
+  glRotatef(thetax, 1, 0, 0);  
+  
+  glutSolidCube(2);  
+  
   glutSwapBuffers();
 }
 
@@ -33,16 +56,52 @@ void reshape(int width, int height) {
   glMatrixMode(GL_MODELVIEW);  
 }
 
+void mouse(int button, int state, int x, int y) {
+  if(state == GLUT_UP)
+    ::button = -1;
+  else {
+    mousey = -y;
+    mousex = x;
+    ::button = button;
+  }
+}
+
+void motion(int x, int y) {
+  y = -y;
+  int dy = y - mousey;
+  int dx = x - mousex;
+  
+  switch(button) {
+    case GLUT_LEFT_BUTTON:
+      thetax -= ROTFACTOR * dy;
+      thetay += ROTFACTOR * dx;
+      break;      
+    case GLUT_MIDDLE_BUTTON:
+      pan -= ROTFACTOR * dy;
+      tilt += ROTFACTOR * dy;
+      break;      
+    case GLUT_RIGHT_BUTTON:
+      int delta = ABS(dx) > ABS(dy) ? dx : dy;
+      approach += XLATEFACTOR * delta;
+      break;
+  }
+  
+  glutPostRedisplay();
+  
+  mousex = x;
+  mousey = y;
+}
+
 int main(int argc, char** argv) {
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
   glutInitWindowSize(WIDTH, HEIGHT);
   glutCreateWindow("OpenGL Viewer");
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
   // keyboard
-  // mouse
-  // motion
+  glutMouseFunc(mouse);
+  glutMotionFunc(motion);
   init();
   glutMainLoop();
 }
